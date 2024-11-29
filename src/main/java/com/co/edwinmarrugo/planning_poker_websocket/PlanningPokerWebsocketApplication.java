e83c31758be60bc09fad30721b43767d0b3d3790
edwin <edwinmarr@hotmail.com>
2024-11-28 22:06:54 -0500

    Initial commit

---
+++ src/main/java/com/co/edwinmarrugo/planning_poker_websocket/PlanningPokerWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.co.edwinmarrugo.planning_poker_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/edwinmarrugo/planning_poker_websocket/PlanningPokerWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.edwinmarrugo.planning_poker_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
